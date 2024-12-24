92efccac72b99a8aa9b97a71e53bc9b36f561b25
Blane <80710483+CSUltiGod99@users.noreply.github.com>
2024-12-24 19:55:47 +0000

    Update sortingSpeed_Enume.js

--- sortingSpeed_Enume.js
+++ sortingSpeed_Enume.js
@@ -1,14 +1,13 @@
-Function IEnumerationI()
+Function IEnumerationIDMStatsMale()
 {
-      
-    h
-    const r
-
+    const DMStars_Male = {"Thomas Shane Skeffington", "Steven Miller"};
+    console.log(DMStars_Male);
 }
-
 
 Function Main()
 {
-
+    const DMStar_Finalists = {"Thomas Davison", "Ellie Donaghue", "Thomas Carlson"};  
+    console.log(DMStar_Finalists);
+    const TapDancing = {"Michael", "Thomas Davison", "Ellie Donaghue"};
+    console.log(TapDancing);
 }
-
